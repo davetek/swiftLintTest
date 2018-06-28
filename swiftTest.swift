75f1ccfe6fdefa2a290604a3c6d4eccf0d97bb32
davetek <dlawrence4994@gmail.com>
2018-06-28 19:33:38 -0400

    modified with code to reproduce SwiftLint bug 1277

--- swiftTest.swift
+++ swiftTest.swift
@@ -1,3 +1,14 @@
 #!/usr/bin/swift
 
-print("Hello")
+struct FooBar {
+    
+    var stuff: [String: String] = ["a" : "apple", "k": "kiwi", "j": "jackfruit"]
+    
+    static let foo = true
+    static let bar = false
+    
+    func baz(lhs: Int, rhs : Int) -> Bool {
+        typealias Type = FooBar
+        return lhs > rhs ? Type.foo : Type.bar
+    }
+}
