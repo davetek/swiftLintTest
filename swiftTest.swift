33ee804b9f773908bcc065e7a0b2eab4cd28249c
davetek <dlawrence4994@gmail.com>
2018-06-30 09:52:43 -0400

    included other scenarios

--- swiftTest.swift
+++ swiftTest.swift
@@ -2,12 +2,17 @@
 
 struct FooBar {
     
-    var stuff: [String: String] = ["a" : "apple", "k": "kiwi", "j": "jackfruit"]
+    var stuff: [String: String] = ["a": "apple", "k": "kiwi", "m": "mango"]
     
     static let foo = true
     static let bar = false
-    
-    func baz(lhs: Int, rhs : Int) -> Bool {
+
+    //as a comparison, the following function w/o typealias does NOT trigger the colon rule 
+    func bazNoAlias(lhs: Int, rhs: Int) -> Bool {
+    	return lhs > rhs ? FooBar.foo : FooBar.bar
+    }
+
+    func baz(lhs: Int, rhs: Int) -> Bool {
         typealias Type = FooBar
         return lhs > rhs ? Type.foo : Type.bar
     }
